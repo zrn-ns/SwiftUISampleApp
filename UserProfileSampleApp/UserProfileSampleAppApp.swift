f7c655f00da57299fb63ff9d434a4c01242f9211
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2022-07-30 16:38:44 +0900

    Initial Commit

---
+++ UserProfileSampleApp/UserProfileSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserProfileSampleAppApp.swift
+//  UserProfileSampleApp
+//
+//  Created by zrn_ns on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct UserProfileSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
