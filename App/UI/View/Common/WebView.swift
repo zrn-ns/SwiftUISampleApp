462013c318317c022b827aa4ccd5f6750b07d769
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2022-08-08 12:36:12 +0900

    ADD WKWebViewをSwiftUIで使えるようにする

---
+++ App/UI/View/Common/WebView.swift
@@ -0,0 +1,21 @@
+//
+//  WebView.swift
+//  App
+//
+//  Created by zrn_ns on 2022/08/08.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    var url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(URLRequest(url: url))
+    }
+}
