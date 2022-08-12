abbcd18b4e8742c20dd37cffa574e8d89dc763ef
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2022-08-12 18:15:46 +0900

    FIX parametersがうまくエンコードできてなかったので渡し方を修正 & 共通化

--- APIClient/APIClient.swift
+++ APIClient/APIClient.swift
@@ -10,30 +10,30 @@ import Alamofire
 
 public struct APIClient {
     public static func send<Req: APIRequest>(_ request: Req, handler: ((Result<Req.Response, APIError>) -> Void)?) {
-        AF.request(request.url,
-                   method: request.method.toHTTPMethod(),
-                   parameters: request.params,
-                   encoder: .urlEncodedForm,
-                   headers: nil,
-                   interceptor: nil,
-                   requestModifier: nil).responseDecodable(of: Req.Response.self) { response in
+        request.asAFDataRequest().responseDecodable(of: Req.Response.self) { response in
             handler?(response.result.mapError { APIError(afError: $0) })
         }
     }
 
     public static func sendAsync<Req: APIRequest>(_ request: Req) async throws -> Req.Response {
-        let result = await AF.request(request.url,
-                                      method: request.method.toHTTPMethod(),
-                                      parameters: request.params,
-                                      encoder: .urlEncodedForm,
-                                      headers: nil,
-                                      interceptor: nil,
-                                      requestModifier: nil).serializingDecodable(Req.Response.self).result
+        let result = await request.asAFDataRequest().serializingDecodable(Req.Response.self).result
         switch result {
         case .success(let response):
             return response
         case .failure(let afError):
             throw APIError(afError: afError)
         }
+    }
+}
+
+extension APIRequest {
+    func asAFDataRequest() -> DataRequest {
+        AF.request(url,
+                   method: method.toHTTPMethod(),
+                   parameters: params,
+                   encoder: URLEncodedFormParameterEncoder(destination: .methodDependent),
+                   headers: nil,
+                   interceptor: nil,
+                   requestModifier: nil)
     }
 }
