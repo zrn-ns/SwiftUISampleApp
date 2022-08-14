198dbe2d59dff89ce5194ec6219f537afa7a0d9e
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2022-08-14 21:05:47 +0900

    ADD PagingParamを定義
    - Paging情報を保持するための型を定義

---
+++ APIClient/Model/PagingParam.swift
@@ -0,0 +1,13 @@
+//
+//  PagingParam.swift
+//  APIClient
+//
+//  Created by zrn_ns on 2022/08/14.
+//
+
+import Foundation
+
+public struct PagingParam {
+    public let currentPage: Int
+    public let hasNextPage: Bool
+}
