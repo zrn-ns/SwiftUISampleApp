15e479e005cd4b5b88d0a0be05dbe6b8057ca475
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2022-08-27 16:25:14 +0900

    MOD APIErrorの分類を厳密に行う
    - 正しい分類になっている自信はない...

--- APIClient/APIError.swift
+++ APIClient/APIError.swift
@@ -13,14 +13,47 @@ public enum APIError: Error, Equatable {
     case connectionFailure
     /// クライアント側のロジックエラー
     case logicError(systemErrorInfo: String)
+    /// ユーザの入力ミス
+    case userInputError(systemErrorInfo: String)
     /// キャンセルされた
     case cancelled
     /// その他
     case other
 
     init(afError: AFError) {
-        #warning("あとでちゃんと実装")
-        print(afError)
+        switch afError {
+        case .createUploadableFailed(let error),
+                .createURLRequestFailed(let error),
+                .downloadedFileMoveFailed(let error, _, _),
+                .requestAdaptationFailed(let error):
+            self = .logicError(systemErrorInfo: error.localizedDescription)
+        case .explicitlyCancelled:
+            self = .cancelled
+        case .invalidURL(let url):
+            self = .userInputError(systemErrorInfo: "invalid url: \(url)")
+        case .multipartEncodingFailed(let reason):
+            self = .logicError(systemErrorInfo: "\(reason)")
+        case .parameterEncodingFailed(let reason):
+            self = .logicError(systemErrorInfo: "\(reason)")
+        case .parameterEncoderFailed(let reason):
+            self = .logicError(systemErrorInfo: "\(reason)")
+        case .requestRetryFailed(let retryError, _):
+            self = .logicError(systemErrorInfo: retryError.localizedDescription)
+        case .responseValidationFailed(let reason):
+            self = .logicError(systemErrorInfo: "\(reason)")
+        case .responseSerializationFailed(let reason):
+            self = .logicError(systemErrorInfo: "\(reason)")
+        case .serverTrustEvaluationFailed(let reason):
+            self = .logicError(systemErrorInfo: "\(reason)")
+        case .sessionDeinitialized:
+            self = .connectionFailure
+        case .sessionInvalidated(_):
+            self = .connectionFailure
+        case .sessionTaskFailed(_):
+            self = .connectionFailure
+        case .urlRequestValidationFailed(let reason):
+            self = .logicError(systemErrorInfo: "\(reason)")
+        }
         self = .other
     }
 }
